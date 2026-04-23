b60e73abab019ef419c9e71b5a4aecda1ed265ff
naveenallu <naveenallu@naveenallus-MacBook-Air.local>
2026-04-23 17:11:20 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/favicon.ico",
+        destination: "/icon",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
